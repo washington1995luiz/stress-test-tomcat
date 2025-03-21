9e1853c7ef0b180ed95ab96b3e98dad5851ac53b
Washington <washington195luiz@gmail.com>
2025-03-21 18:02:03 -0300

    add github actions and another controller

---
+++ src/main/java/br/com/washington/stress_test_tomcat/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package br.com.washington.stress_test_tomcat.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String greeting(){
+        return "Welcome to my application!";
+    }
+}
--- src/main/java/br/com/washington/stress_test_tomcat/controllers/StressController.java
+++ src/main/java/br/com/washington/stress_test_tomcat/controllers/StressController.java
@@ -3,10 +3,7 @@ package br.com.washington.stress_test_tomcat.controllers;
 import br.com.washington.stress_test_tomcat.models.Stress;
 import br.com.washington.stress_test_tomcat.services.StressService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
